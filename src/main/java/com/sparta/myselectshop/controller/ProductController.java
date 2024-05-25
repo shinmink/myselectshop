d22c2750e13e5312afb7c7f9550712f7beae5898
shinmink <sinmk99@hansung.ac.kr>
2024-05-25 21:28:44 +0900

    상품 페이징과 정렬

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,15 +33,13 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<ProductResponseDto> getProduct(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-
-        return productService.getProduct(userDetails.getUser());
-
-    }
-
-    @GetMapping("/admin/products")
-    public List<ProductResponseDto> getAllProduct() {
-        return productService.getAllProduct();
+    public Page<ProductResponseDto> getProduct(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return productService.getProduct(userDetails.getUser(), page-1, size, sortBy, isAsc);
     }
 
 
